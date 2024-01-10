a99398e5dc2908d93a5dd4ed4f70cda14cbf6e3c
MonaaEid <monaa_eid@hotmail.com>
2024-01-10 19:58:03 +0200

    add bash script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,7 +12,7 @@ mkdir -p /data/web_static/releases/test/
 touch /data/web_static/releases/test/index.html
 echo "<html><head></head><body><h4>hello folks</h4></body></html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data
+chown -hR ubuntu:ubuntu /data
 
 sed -i '/listen 80 default_server;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
