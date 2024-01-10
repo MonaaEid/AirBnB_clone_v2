54e6a4a9f828f249a990f3c6f2b2b127d05db8c3
MonaaEid <monaa_eid@hotmail.com>
2024-01-10 19:13:58 +0200

    add bash script

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Bash script that sets up your web servers for the deployment of web_static
+if ! [ -x "$(command -v nginx)" ]; then
+    apt-get update
+    apt-get intall -y ngnix
+fi
+# mkdir /data/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+touch /data/web_static/releases/test/index.html | echo "<html><head></head><body><h4>hello folks</h4></body></html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R:  ubuntu:ubuntu /data
+
+sudo sed -i '/listen 80 default_server;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+sudo service nginx restart
