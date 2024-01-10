49ed2e08697d33a2a50a63f44ac9d7a3b80c9d84
MonaaEid <monaa_eid@hotmail.com>
2024-01-10 21:13:12 +0200

    add bash script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static.
-apt-get update
+apt-get update -y
 apt-get intall -y ngnix
 # mkdir /data/
 mkdir -p /data/web_static/
 mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-touch /data/web_static/releases/test/index.html
-echo "<html><head></head><body><h4>hello folks</h4></body></html>" > /data/web_static/releases/test/index.html
+# touch /data/web_static/releases/test/index.html
+echo "<html><head></head><body><h4>hello folks</h4></body></html>" | tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data
 
-sed -i '/listen 80 default_server;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-
-service nginx restart
+# sed -i '/listen 80 default_server;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
+sudo service nginx restart
