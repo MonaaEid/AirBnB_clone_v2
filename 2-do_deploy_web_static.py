5b760c0aa29f7935f502e83ac0d3eddc21decc0f
MonaaEid <monaa_eid@hotmail.com>
2024-01-11 19:51:01 +0200

    pack web static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,26 +7,21 @@ env.hosts = ['54.90.1.129', '18.208.119.233']
 
 
 def do_deploy(archive_path):
-    """deploy the archived code"""
-    if not exists(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-
     try:
-        put(archive_path, "/tmp/")
-
-        archive_filename = archive_path.split("/")[-1]
-        archive_name_no_ext = archive_filename.split(".")[0]
-        run("sudo mkdir -p /data/web_static/releases/{}/".format(archive_name_no_ext))
-        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
-            archive_filename, archive_name_no_ext))
-
-        run("sudo rm /tmp/{}".format(archive_filename))
-
-        run("sudo rm -f /data/web_static/current")
-
-        run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current".format(archive_name_no_ext))
-
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-
-    except Exception:
+    except:
         return False
