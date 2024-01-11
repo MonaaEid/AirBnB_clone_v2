682c4de4f6512d4ba861ffc7b6a4a7a7ffe5743a
MonaaEid <monaa_eid@hotmail.com>
2024-01-11 20:07:14 +0200

    pack web static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,16 +12,16 @@ def do_deploy(archive_path):
         return False
     try:
         file_n = archive_path.split("/")[-1]
-        archive_name_no_ext = file_n.split(".")[0]
+        archv_no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, archive_name_no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, archive_name_no_ext))
+        run('mkdir -p {}{}/'.format(path, archv_no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, archv_no_ext))
         run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, archive_name_no_ext))
-        run('rm -rf {}{}/web_static'.format(path, archive_name_no_ext))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, archv_no_ext))
+        run('rm -rf {}{}/web_static'.format(path, archv_no_ext))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, archive_name_no_ext))
+        run('ln -s {}{}/ /data/web_static/current'.format(path, archv_no_ext))
         return True
     except BaseException:
         return False
