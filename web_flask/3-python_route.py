47dc8fb40e16f64d2cdcb34b84ed053c45591398
MonaaEid <monaa_eid@hotmail.com>
2024-01-26 17:18:38 +0200

    Add app files

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,9 +21,9 @@ def c_text(text):
     return'C {}'.format(text.replace('_', ' '))
 
 
-@app.route('/python/', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text="is_cool"):
+def python_text(text="is cool"):
     return'python {}'.format(text.replace('_', ' '))
 
 
