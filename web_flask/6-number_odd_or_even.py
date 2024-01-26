cfc9b9f730a020a81ba5e56a3c99efc2e59fd23d
MonaaEid <monaa_eid@hotmail.com>
2024-01-26 22:40:41 +0200

    Add app files

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -39,11 +39,12 @@ def num_template(n):
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def odd_or_even(n):
-    if (( n > 0 ) & (n % 2 ==0)):
+    if ((n > 0) & (n % 2 == 0)):
         txt = "Number: "+str(n) + " is even"
-    if (( n > 0 ) & (n % 2 != 0)):
+    if ((n > 0) & (n % 2 != 0)):
         txt = "Number: "+str(n) + " is odd"
     return render_template('6-number_odd_or_even.html', txt=txt)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
