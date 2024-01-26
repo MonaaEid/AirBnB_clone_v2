0465c6e7271e782a38a692ee3aac99bdc3d02670
MonaaEid <monaa_eid@hotmail.com>
2024-01-26 17:03:44 +0200

    Add app files

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello_world():
     """start the app"""
-    return "Hello HBNB"
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
