285149c142dcedf8f384c4c14beb1648ee6d2894
MonaaEid <monaa_eid@hotmail.com>
2024-01-27 17:07:43 +0200

    Add app files

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """script that starts a Flask web application"""
 from flask import Flask, render_template
-from models import storage, State, City
-
+from models import storage, State
+from models.state import State
 app = Flask(__name__)
 
 
