82f26370ea65647c1d75618c65348854961d9558
MonaaEid <monaa_eid@hotmail.com>
2024-01-28 16:12:49 +0200

    Add app files

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -16,12 +16,11 @@ def states():
 
 @app.route("/states/<id>", strict_slashes=False)
 def states_id(id):
-    """comment"""
+    """comment 2"""
     for state in storage.all(State).values():
         if state.id == id:
             return render_template("9-states.html", state=state)
     return render_template("9-states.html")
-    # return render_template("9-states.html")
 
 
 @app.teardown_appcontext
